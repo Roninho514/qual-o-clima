b99f4869f61ef78d8fef37bf56b3f4205309f3c3
Roni dos Santos Silva <82835587+Roninho514@users.noreply.github.com>
2025-04-11 13:32:50 -0300

    adjust layout

--- qual-o-clima/src/app/components/home/home.component.ts
+++ qual-o-clima/src/app/components/home/home.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ClimateService } from '../../service/climate.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
   constructor(private climateService: ClimateService){}
-  clima : any;
+  clima : number[] = [1, 2, 3, 4, 5];
+  inputCity : string = '';
 
   ngOnInit(): void {
     /* this.climateService.getClimate().subscribe((data) => {
